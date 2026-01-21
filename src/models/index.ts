446f238f98a5a52469fcc9630f2b246192e69083
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-21 16:16:10 +0500

    new: Better-Auth models setup

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+// Module Exports.
+export { default as User } from './user';
+export { default as Account } from './account';
+export { default as Session } from './session';
+export { default as Verification } from './verification';
