b0543fa3a1afcee6f5c595d07d9345afe405b842
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-21 16:18:04 +0500

    fix: model filename convention

--- src/models/index.model.ts
+++ src/models/index.model.ts
@@ -1,5 +1,5 @@
 // Module Exports.
-export { default as User } from './user';
-export { default as Account } from './account';
-export { default as Session } from './session';
-export { default as Verification } from './verification';
+export { default as User } from './user.model';
+export { default as Account } from './account.model';
+export { default as Session } from './session.model';
+export { default as Verification } from './verification.model';
