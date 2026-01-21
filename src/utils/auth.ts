d3b91358688c9131b5a674593bd38f4bac162653
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-21 13:59:09 +0500

    update: MongoDB and Better-Auth Integration

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,11 @@
 // Lib Imports.
 import { betterAuth } from 'better-auth';
+import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 
-export const auth = betterAuth({});
+// Local Imports.
+import { database } from './config';
+
+// Better Auth configuration.
+export const auth = betterAuth({
+  database: mongodbAdapter(database.connection.db!),
+});
