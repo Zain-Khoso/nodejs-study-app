10d258a4d08d7478f59469cefd934e75f9e2c6ab
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-21 15:58:57 +0500

    update: Better-Auth -> Express integration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 // Lib Imports.
 import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
 
 // Local Imports.
 import { env } from './utils/config';
+import { auth } from './utils/auth';
 
 // Express Server Definition.
 const app = express();
+
+// Authentication.
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 // Dummy Route.
 app.get('/', (_req, res) => res.send('Hello World!'));
