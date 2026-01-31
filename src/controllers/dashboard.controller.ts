9e70266f4ec90112720ed694142eded9da962708
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-31 11:31:03 +0500

    update: getDashboard controller

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,8 +1,14 @@
 // Lib Imports.
 import { Request, Response } from 'express';
 
+// Local Imports.
+import { serializeResponse } from '../utils/serializers';
+
 // Controller to get data for dashboard page.
-async function getDashboard(req: Request, res: Response) {}
+async function getDashboard(req: Request, res: Response) {
+  if (req.user) return res.status(200).json(serializeResponse(req.user));
+  else return res.status(400).json(serializeResponse({}, { message: 'User not found' }));
+}
 
 // Exports.
 export default { getDashboard };
