31d72551433b29fa99661fa755828380861a75e6
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-31 11:04:46 +0500

    fix: centeredLog func

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,8 @@
 // Helper function used to for console logs.
 function centeredLog(str: string, width: number = 120, char: string = '-') {
-  return str.padStart(Math.floor((width - str.length) / 2), char).padEnd(width, char);
+  return console.log(
+    str.padStart(str.length + Math.floor((width - str.length) / 2), char).padEnd(width, char)
+  );
 }
 
 // Exports.
