20c6302e0d003030d7b770bf78831d4812d5e46c
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-31 11:27:11 +0500

    fix: User type in request object

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -6,7 +6,7 @@ declare global {
   namespace Express {
     interface Request extends BaseRequest {
       session: typeof auth.$Infer.Session.session | null;
-      user: typeof auth.$Infer.User.user | null;
+      user: typeof auth.$Infer.Session.user | null;
     }
   }
 }
